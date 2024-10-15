3d44108ec1d8afd09b7191366e6d64014cc37288
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2024-10-16 01:19:21 +0600

    adding basic registration

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotnev = require("dotenv");
 
+const registrationRoute = require("./routes/auth/registration");
+
 dotnev.config();
 
 const app = express();
@@ -10,6 +12,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Successfully contected to database"))
   .catch((error) => console.log(`Couldn't connect to database : ${error}`));
+
+app.use(express.json());
+
+app.use(registrationRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
---
+++ routes/auth/registration.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../../models/User");
+
+const router = express.Router();
+
+router.post("/api/auth/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ msg: "An account already exists with the given Email" });
+    }
+    user = new User({
+      name,
+      email,
+      password,
+    });
+    await user.save();
+    res.status(201).json(user);
+  } catch {
+    (error) => console.log(`User registration failed : ${error.message}`);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
