1c2ba7109099b20653d2bb95444f25d058271ee5
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2024-10-16 21:52:31 +0600

    minor name change

--- routes/stationRoutes.js
+++ routes/stationRoutes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const {
   createStation,
   getStations,
+  updateStation,
+  deleteStation,
 } = require("../controllers/stationController");
 const router = express.Router();
 
 router.post("/create", createStation);
 router.get("/", getStations);
+router.put("/update/:id", updateStation);
+router.delete("/delete/:id", deleteStation);
 
 module.exports = router;
